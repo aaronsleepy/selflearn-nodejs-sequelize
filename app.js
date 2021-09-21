db92e7eee7a9b1bb1feb4f722fefea6af4f97e93
aaronsleepy <aaron@kmong.com>
2021-09-21 20:14:52 +0900

    task: Add sequlize db connect

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const { sequelize } = require('./models/index.js');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(reason => {
+    console.error(reason);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded( {extended: false }));
+
+app.use((req, res, next) => {
+  const error = new Error(`No router for ${req.method}, ${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`);
+});
